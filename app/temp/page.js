4999c08be8a394eb82fcb2d7be5bea1698afdcfd
Abhishek Singhal <abhisinghal112001@gmail.com>
2023-09-09 09:19:19 +0530

    ar

--- app/temp/page.js
+++ app/temp/page.js
@@ -1,21 +1,40 @@
 "use client"
-import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
-import { Canvas } from '@react-three/fiber'
+// components/ARScene.js
+import React, { useRef } from 'react';
+import { ARButton, XR, Hands } from '@react-three/xr';
+import { Canvas, useFrame } from '@react-three/fiber';
 
-export default function page() { 
+const ARScene = () => {
+  const arRef = useRef();
+
+  // Function to add a cube to the scene when AR is ready
+  const addCubeToScene = (scene) => {
+    const cubeGeometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'blue' });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(0, 0.05, -0.2); // Adjust the cube's position
+    scene.add(cube);
+  };
+
+  useFrame(({ scene }) => {
+    // Check if AR is ready
+    if (arRef.current) {
+      // Add the cube to the scene once AR is ready
+      addCubeToScene(scene);
+    }
+  });
+
   return (
     <>
-      <ARButton />
+      <ARButton ar={arRef} />
       <Canvas>
         <XR>
-          <Controllers />
           <Hands />
-          <mesh>
-            <boxGeometry />
-            <meshBasicMaterial color="blue" />
-          </mesh>
         </XR>
       </Canvas>
     </>
-  )
-}
+  );
+};
+
+export default ARScene;
+
