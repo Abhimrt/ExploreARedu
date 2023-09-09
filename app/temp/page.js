31c19b0564dc73f552d2dcbdbff8923462daa537
Abhishek Singhal <abhisinghal112001@gmail.com>
2023-09-09 08:28:05 +0530

    ar

---
+++ app/temp/page.js
@@ -0,0 +1,21 @@
+"use client"
+import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { Canvas } from '@react-three/fiber'
+
+export default function page() {
+  return (
+    <>
+      <VRButton />
+      <Canvas>
+        <XR>
+          <Controllers />
+          <Hands />
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
+  )
+}
